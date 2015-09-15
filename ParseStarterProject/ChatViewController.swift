547e561bfd9be14b49e0db6d9626f087bbf80d81
Churo Chu <churocat@gmail.com>
2015-09-15 18:33:22 +0800

    add swipe segue

---
+++ ParseStarterProject/ChatViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ChatViewController.swift
+//  ParseStarterProject-Swift
+//
+//  Created by 竹田 on 2015/9/15.
+//  Copyright (c) 2015年 Parse. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func swipeDown(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
